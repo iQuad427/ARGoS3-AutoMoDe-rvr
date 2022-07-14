33390b255cd0dc8d9e128bcab3d197a876682d58
rafftod <ramaluto@gmail.com>
2022-07-14 16:20:08 +0200

    Fix modules

--- src/modules/AutoMoDeCondition.cpp
+++ src/modules/AutoMoDeCondition.cpp
@@ -1,27 +1,32 @@
 /**
-  * @file <src/modules/AutoMoDeCondition.cpp>
-  *
-  * @author Antoine Ligot - <aligot@ulb.ac.be>
-  *
-  * @package ARGoS3-AutoMoDe
-  *
-  * @license MIT License
-  */
+ * @file <src/modules/AutoMoDeCondition.cpp>
+ *
+ * @author Antoine Ligot - <aligot@ulb.ac.be>
+ *
+ * @package ARGoS3-AutoMoDe
+ *
+ * @license MIT License
+ */
 
 #include "AutoMoDeCondition.h"
 
-namespace argos {
+namespace argos
+{
 
 	/****************************************/
 	/****************************************/
 
-	const std::string AutoMoDeCondition::GetDOTDescription() {
+	const std::string AutoMoDeCondition::GetDOTDescription()
+	{
 		std::stringstream ss;
 		ss << m_strLabel;
-		if (!m_mapParameters.empty()) {
+		if (!m_mapParameters.empty())
+		{
 			std::map<std::string, Real>::iterator it;
-			for (it = m_mapParameters.begin(); it != m_mapParameters.end(); it++) {
-				ss << "\\n" << it->first << "=" << it->second ;
+			for (it = m_mapParameters.begin(); it != m_mapParameters.end(); it++)
+			{
+				ss << "\\n"
+				   << it->first << "=" << it->second;
 			}
 		}
 		return ss.str();
@@ -30,42 +35,48 @@ namespace argos {
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeCondition::AddParameter(const std::string& str_identifier, const Real& f_value) {
+	void AutoMoDeCondition::AddParameter(const std::string &str_identifier, const Real &f_value)
+	{
 		m_mapParameters.insert(std::pair<std::string, Real>(str_identifier, f_value));
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const UInt32& AutoMoDeCondition::GetOrigin() const {
+	const UInt32 &AutoMoDeCondition::GetOrigin() const
+	{
 		return m_unFromBehaviourIndex;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const UInt32& AutoMoDeCondition::GetExtremity() const {
+	const UInt32 &AutoMoDeCondition::GetExtremity() const
+	{
 		return m_unToBehaviourIndex;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeCondition::SetOrigin(const UInt32& un_from) {
+	void AutoMoDeCondition::SetOrigin(const UInt32 &un_from)
+	{
 		m_unFromBehaviourIndex = un_from;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeCondition::SetExtremity(const UInt32& un_to) {
+	void AutoMoDeCondition::SetExtremity(const UInt32 &un_to)
+	{
 		m_unToBehaviourIndex = un_to;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeCondition::SetOriginAndExtremity(const UInt32& un_from, const UInt32& un_to) {
+	void AutoMoDeCondition::SetOriginAndExtremity(const UInt32 &un_from, const UInt32 &un_to)
+	{
 		m_unFromBehaviourIndex = un_from;
 		m_unToBehaviourIndex = un_to;
 	}
@@ -73,56 +84,64 @@ namespace argos {
 	/****************************************/
 	/****************************************/
 
-	const std::string& AutoMoDeCondition::GetLabel() const {
+	const std::string &AutoMoDeCondition::GetLabel() const
+	{
 		return m_strLabel;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeCondition::SetIndex(const UInt32& un_index) {
+	void AutoMoDeCondition::SetIndex(const UInt32 &un_index)
+	{
 		m_unIndex = un_index;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const UInt32& AutoMoDeCondition::GetIndex() const {
+	const UInt32 &AutoMoDeCondition::GetIndex() const
+	{
 		return m_unIndex;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	void AutoMoDeCondition::SetIdentifier(const UInt32& un_id) {
+	void AutoMoDeCondition::SetIdentifier(const UInt32 &un_id)
+	{
 		m_unIdentifier = un_id;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	const UInt32& AutoMoDeCondition::GetIdentifier() const {
+	const UInt32 &AutoMoDeCondition::GetIdentifier() const
+	{
 		return m_unIdentifier;
 	}
 
 	/****************************************/
 	/****************************************/
 
-	std::map<std::string, Real> AutoMoDeCondition::GetParameters() const {
+	std::map<std::string, Real> AutoMoDeCondition::GetParameters() const
+	{
 		return m_mapParameters;
 	}
 
-  /****************************************/
-	/****************************************/
-
-  void AutoMoDeCondition::SetRobotDAO(EpuckDAO* pc_robot_dao) {
-      m_pcRobotDAO = pc_robot_dao;
-  }
-
 	/****************************************/
 	/****************************************/
 
-  bool AutoMoDeCondition::EvaluateBernoulliProbability(const Real& f_probability) const {
+	void AutoMoDeCondition::SetRobotDAO(RVRDAO *pc_robot_dao)
+	{
+		m_pcRobotDAO = pc_robot_dao;
+	}
+
+	/****************************************/
+	/****************************************/
+
+	bool AutoMoDeCondition::EvaluateBernoulliProbability(const Real &f_probability) const
+	{
 		return m_pcRobotDAO->GetRandomNumberGenerator()->Bernoulli(f_probability);
 	}
 
